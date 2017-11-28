ac0b916440b0c7dc7e33e27a6ecbf98cf33500dd
Jérôme Charron <jerome.charron@gmail.com>
2017-11-28 22:22:59 +0100

    In sync with the full version + added some needed (empty) functions

--- basic/main.js
+++ basic/main.js
@@ -1,12 +1,14 @@
 // The number of alive cells
-var nbCells = random(100, 1000);
+var nbCells;
 
 // Setup Canvas
+var width = height = 100;
 var canvas = document.querySelector('canvas');
+canvas.width = width;
+canvas.height = height;
 var nbCellsSpan = document.querySelector('span#nb-cells');
 var numCycleSpan = document.querySelector('span#num-cycle');
 var ctx = canvas.getContext('2d');
-var width = height = 100;
 var area = Array(width);
 
 
@@ -17,16 +19,28 @@ function random(min, max) {
 }
 
 
+// Init our Game Of Life
+function init() {
+  // ...
+}
+
+
 // Draw a cell
 function drawCell(x, y, state) {
   ctx.beginPath();
-  if (state == 0) {
-    ctx.fillStyle="#FFFFFF";
-  } else {
+  if (state == 1) {
     ctx.fillStyle="#000000";
+  } else {
+    ctx.fillStyle="#FFFFFF";
   }
-  ctx.fillRect(x, y, 1, 1);  
+  ctx.fillRect(x, y, 1, 1);
   ctx.stroke();
+}
+
+
+// Count number of neighbours for a given cell
+function countNeighbours(x, y) {
+  // ...
 }
 
 
@@ -40,16 +54,16 @@ function updateCells() {
 // ************************************
 
 // Init the Game
-// ...
+init();
 
 // Add life to cells
 var cycle = 0;
 function loop() {
-  updateCells();
+  area = updateCells();
+  // stop the the animation if it runs out-of-canvas
   if (cycle++ >= 1000) {
     clearInterval(myTimer);
   }
   numCycleSpan.textContent = cycle;
 }
-var myTimer=setInterval(loop, 1); 
-
+var myTimer=setInterval(loop, 200);
